1ece4a744347b1ee33ab2c909736f24eb081afff
Tuna195 <tuan19052005@gmail.com>
2026-03-13 22:52:05 +0700

    Time: 2 ms (54.32%), Space: 10.4 MB (13.61%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for(auto move : moves)
+        {
+            if (move == 'R') x++;
+            if (move == 'L') x--;
+            if (move == 'U') y++;
+            if (move == 'D') y--;
+        }
+        return x==0 && y==0;
+    }
+};
