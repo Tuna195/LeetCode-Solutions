094116d7c32d7db50a8b9b35a1c5f5dc1f4f9e69
Tuna195 <tuan19052005@gmail.com>
2026-03-13 22:50:52 +0700

    Time: 12 ms (83.14%), Space: 24.3 MB (92.82%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> uo_mp;
-        vector<vector<string>> ans;
-        for(string s : strs){
-            string ss = s;
-            sort(s.begin(), s.end());
-            if(uo_mp.find(s) == uo_mp.end()){
-                uo_mp[s] = {ss};
-            }
-            else{
-                uo_mp[s].push_back(ss);
-            }
-        }
-        for(auto it = uo_mp.begin(); it != uo_mp.end(); it++){
-            ans.push_back(it->second);
-        }
-        return ans;
+    unordered_map<string, vector<string>> mp;
+	for(const auto &str : strs){
+		string s = str;
+		sort(s.begin(), s.end());
+		mp[s].push_back(str);
+	}
+	int n = mp.size();
+	vector<vector<string>> v(n);
+	int i = 0;
+	for(const auto &pair : mp){
+		v[i] = pair.second;
+        i++;
+	}
+	return v;
     }
 };
